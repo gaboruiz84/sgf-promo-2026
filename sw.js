4bf1e34f4bb37db0426f0b0129676895205902ea
gaboruiz84 <abner.ruiz.martinez@clases.edu.sv>
2026-03-03 16:01:25 -0600

    Update 2 sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // === CAMBIA ESTE NÚMERO CADA VEZ QUE HAGAS UNA ACTUALIZACIÓN (ej. v2, v3, v4...) ===
-const CACHE_NAME = 'sgf-promo-v8'; 
+const CACHE_NAME = 'sgf-promo-v11'; 
 
 const urlsToCache = [
   './',
